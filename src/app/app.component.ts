8bcfc036f976fcf983dd10b437445b092c5a9b09
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-03-12 00:08:55 +0530

    [Shruti] Add. User can input name with restrictions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Hello from BridgeLabz';
-  logoUrl = 'assets/logo.jpeg';
-  bridgeLabzUrl = 'https://www.bridgelabz.com';
+  userName: string = '';
+  errorMessage: string = '';
 
-  openBridgeLabzSite() {
-    window.open(this.bridgeLabzUrl, '_blank');
+  validateName(): void {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+    if (!this.userName.match(namePattern)) {
+      this.errorMessage = 'Name must start with a capital letter and have at least 3 characters.';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+
+  openBridgeLabz(): void {
+    window.open('https://www.bridgelabz.com', '_blank');
   }
 }
