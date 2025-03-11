bbdac8300c554078f47be6d1978868197fc54562
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-03-11 23:26:30 +0530

    [Shruti] Add. Property Binding to display logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular-JS';
-  logoPath = 'assets/logo.jpeg';
+  title = 'Hello BridgeLabz';
+  logoUrl = 'assets/logo.jpeg';
 }
